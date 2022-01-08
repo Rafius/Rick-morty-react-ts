68c0266a4dbb2d598335647df91a5628da00a0bd
rafa <rafius>
2022-01-08 11:53:00 +0100

    Add filter

--- src/pages/Characters/Characters.tsx
+++ src/pages/Characters/Characters.tsx
@@ -3,20 +3,25 @@ import Character from "../../components/Character";
 import { CharacterType } from "../../types/CharactersTypes";
 import useCharacters from "../../hooks/useCharacters";
 import Pagination from "../../components/Pagination";
+import Input from "../../components/Input";
+import useFilter from "../../hooks/useFilter";
 
 const Characters = () => {
   const { characters, currentPage, handlePaginationClick, paginationButtons } =
     useCharacters();
 
+  const { value, handleInputChange, results } = useFilter(characters.results);
+
   return (
     <div>
+      <Input value={value} handleInputChange={handleInputChange} />
       <Pagination
         paginationButtons={paginationButtons}
         handlePaginationClick={handlePaginationClick}
         currentPage={currentPage}
       />
       <div className="Characters">
-        {characters.results.map((character: CharacterType) => (
+        {results?.map((character: CharacterType) => (
           <Character key={character.id} {...character} />
         ))}
       </div>
