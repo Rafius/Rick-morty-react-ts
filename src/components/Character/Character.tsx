bce53494c7f229ac6061ec5490eec8d446bb5ede
rafa <rafius>
2022-01-08 20:45:44 +0100

    Add character hover

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-import { CharactersType } from "../../types/CharactersTypes";
+import { CharacterType } from "../../types/CharactersTypes";
 import "./Character.css";
 
-const CharacterInfo = ({ id, image, name, status }: CharactersType) => (
+const CharacterInfo = ({ id, image, name, status }: CharacterType) => (
   <Link className="Character" key={id} to={`/character-info/${id}`}>
     <img className="Character-img" loading="lazy" src={image}></img>
     <div className="Character-Name">{name}</div>
