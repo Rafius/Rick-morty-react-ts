e1d49c9f462374d921657990b8d0680e0c3d0159
rafa <rafius>
2022-06-17 16:31:37 +0200

    Update packages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,14 +4,36 @@ import Header from "../components/Header";
 import Characters from "../pages/Characters";
 import CharacterInfo from "../pages/CharacterInfo";
 import Episodes from "../components/Episodes";
+import { Suspense } from "react";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Header />
     <Routes>
-      <Route path="/" element={<Characters />} />
-      <Route path="/character-info/:id" element={<CharacterInfo />} />
-      <Route path="/episodes" element={<Episodes />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback="Loading Characters...">
+            <Characters />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/character-info/:id"
+        element={
+          <Suspense fallback="Loading CharacterInfo...">
+            <CharacterInfo />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/episodes"
+        element={
+          <Suspense fallback="Loading Episodes...">
+            <Episodes />
+          </Suspense>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
